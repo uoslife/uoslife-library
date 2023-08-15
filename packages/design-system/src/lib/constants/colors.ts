bc5a911376c1756854714804fc845b2381b40610
은배 <geb9211@gmail.com>
2023-08-15 18:34:36 +0900

    feat: colors 값 수정

--- packages/design-system/src/lib/constants/colors.ts
+++ packages/design-system/src/lib/constants/colors.ts
@@ -1,55 +1,81 @@
 const white = "#FFFFFF";
 const black = "#000000";
 
-const primary = {
-	light: "#BFDFFF",
-	normal: "#4686FF",
-	strong: "#305ADC",
-};
+// const primary = {
+// 	light: "#BFDFFF",
+// 	normal: "#4686FF",
+// 	strong: "#305ADC",
+// };
 
-const accent = {
-	light: "#FCECB8",
-	normal: "#F8D45E",
-	strong: "#F5B32A",
-};
+// const accent = {
+// 	light: "#FCECB8",
+// 	normal: "#F8D45E",
+// 	strong: "#F5B32A",
+// };
 
-const text = {
-	normal: "#272727",
-	strong: black,
-};
+// const text = {
+// 	normal: "#272727",
+// 	strong: black,
+// };
 
 export const colors = {
 	white,
 	black,
-	grey: text.normal,
 
-	blue50: "#E4F2FF",
-	blue100: primary.light,
-	blue200: "#97CBFF",
-	blue300: "#6DB6FF",
-	blue400: "#53A6FF",
-	blue500: "#4496FF",
-	blue600: primary.normal,
-	blue700: "#4574EA",
-	blue800: primary.strong,
-	blue900: "#3240B7",
-	yellow50: "#FDF8E3",
-	yellow100: accent.light,
-	yellow200: "#FADF8A",
-	yellow300: accent.normal,
-	yellow400: "#F7C941",
-	yellow500: "#F6C02F",
-	yellow600: accent.strong,
-	yellow700: "#F4A125",
-	yellow800: "#F39121",
-	yellow900: "#F27419",
+	primaryBrand: "#4686FF",
+	primaryUi: "#0F6EFB",
+	primaryLight: "#AACEFF",
+	primaryLighter: "#CDE5FF",
+	primaryLighterAlt: "#E9F3FF",
+	primaryDark: "#0646AC",
+	primaryDarker: "#033882",
+	primaryDarkAlt: "#0052E0",
 
-	primaryLight: primary.light,
-	primaryNormal: primary.normal,
-	primaryStrong: primary.strong,
-	accentLight: accent.light,
-	accentNormal: accent.normal,
-	acccentStrong: accent.strong,
-	textNormal: text.normal,
-	textStrong: text.strong,
+	secondaryBrand: "#F8D45E",
+	secondaryUi: "#F9B000",
+	secondaryLight: "#FAE396",
+	secondaryDark: "#B67800",
+
+	grey10: "#FAF9F8",
+	grey20: "#F3F2F1",
+	grey40: "#E1DFDD",
+	grey60: "#C8C6C4",
+	grey90: "#A19F9D",
+	grey130: "#605E5C",
+	grey150: "#2B3A39",
+	grey160: "#323130",
+	grey190: "#201F1E",
+
+	red: "#A80000",
+	// grey: text.normal,
+
+	// blue50: "#E4F2FF",
+	// blue100: primary.light,
+	// blue200: "#97CBFF",
+	// blue300: "#6DB6FF",
+	// blue400: "#53A6FF",
+	// blue500: "#4496FF",
+	// blue600: primary.normal,
+	// blue700: "#4574EA",
+	// blue800: primary.strong,
+	// blue900: "#3240B7",
+	// yellow50: "#FDF8E3",
+	// yellow100: accent.light,
+	// yellow200: "#FADF8A",
+	// yellow300: accent.normal,
+	// yellow400: "#F7C941",
+	// yellow500: "#F6C02F",
+	// yellow600: accent.strong,
+	// yellow700: "#F4A125",
+	// yellow800: "#F39121",
+	// yellow900: "#F27419",
+
+	// primaryLight: primary.light,
+	// primaryNormal: primary.normal,
+	// primaryStrong: primary.strong,
+	// accentLight: accent.light,
+	// accentNormal: accent.normal,
+	// acccentStrong: accent.strong,
+	// textNormal: text.normal,
+	// textStrong: text.strong,
 };
