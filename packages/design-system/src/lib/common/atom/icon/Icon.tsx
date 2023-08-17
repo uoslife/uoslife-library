b8744d2389fdb3953155f965ed820d62510bba52
은배 <geb9211@gmail.com>
2023-08-17 23:11:00 +0900

    feat: 이미지 동적으로 불러오도록 getImageUrl함수 추가

--- packages/design-system/src/lib/common/atom/icon/Icon.tsx
+++ packages/design-system/src/lib/common/atom/icon/Icon.tsx
@@ -6,16 +6,13 @@ export type IconProps = {
 	size: 16 | 20 | 24;
 };
 
+const getImageUrl = (name: string) => {
+	return new URL(`../../../assets/images/icons/${name}.png`, import.meta.url)
+		.href;
+};
+
 export const Icon = ({ name, size }: IconProps) => {
-	return (
-		<S.ImageWrapper
-			source={{
-				uri: `../../../assets/images/icons/${name}.png`,
-			}}
-			size={size}
-		/>
-		// <img src="../../../assets/images/icons/bookmark_grey60.png" />
-	);
+	return <S.ImageWrapper source={require(getImageUrl(name))} size={size} />;
 };
 
 const S = {
