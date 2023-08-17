fe24af9df385f641a26b2017a9f2fc741f971712
은배 <geb9211@gmail.com>
2023-08-18 05:13:37 +0900

    feat: uri로 번들링되도록 코드 변경

--- packages/design-system/src/lib/common/atom/icon/Icon.tsx
+++ packages/design-system/src/lib/common/atom/icon/Icon.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/native";
-import IconsRequireObject from "../../../assets/images/icons";
+// import { IconNameList } from "../../../assets/images/icons";
 import { IconsNameType } from "../../..";
 
 export type IconProps = {
@@ -7,13 +7,31 @@ export type IconProps = {
 	size: 16 | 20 | 24;
 };
 
-// const getImageUrl = (name: string) => {
-// 	return new URL(`../../../assets/images/icons/${name}.png`, import.meta.url)
-// 		.href;
+const getImageUrl = (name: string) => {
+	return new URL(`../../../assets/images/icons/${name}.png`, import.meta.url)
+		.href;
+};
+
+// const IconsRequireObject: IconsRequireObjectType = {
+// 	uoslife_white: { uri: getImageUrl("uoslife_white") },
+// 	cafeteria_primaryDarker: require(getImageUrl("cafeteria_primaryDarker")),
+// 	library_primaryDarker: 0,
+// 	campaign_primaryDarker: 0,
+// 	utility_primaryBrand: 0,
+// 	utility_grey130: 0,
+// 	studentId_primaryBrand: 0,
+// 	studentId_grey130: 0,
+// 	person_primaryBrand: 0,
+// 	person_grey130: 0,
+// 	person_white: 0,
+// 	openInNew_primaryBrand: 0,
+// 	timer_primaryBrand: 0,
+// 	bookmark_primaryBrand: 0,
+// 	bookmark_grey60: 0,
 // };
 
 export const Icon = ({ name, size }: IconProps) => {
-	return <S.ImageWrapper source={IconsRequireObject[name]} size={size} />;
+	return <S.ImageWrapper source={{ uri: getImageUrl(name) }} size={size} />;
 };
 
 const S = {
