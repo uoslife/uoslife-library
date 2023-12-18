71e2265625ab723c1b01dab701a7ae34bed47c4d
EunBae Gong <geb9211@gmail.com>
2023-12-19 04:06:05 +0900

    feat: useThrottle 훅의 export 방식 변경

--- packages/react/src/lib/hooks/useThrottle.ts
+++ packages/react/src/lib/hooks/useThrottle.ts
@@ -4,7 +4,7 @@ import { useRef } from "react";
 
 const THROTTLE_DEFAULT_TIME = 1 * 1000;
 
-const useThrottle = <T extends Function>(
+export const useThrottle = <T extends Function>(
 	callback: T,
 	throttleTime: number | undefined = THROTTLE_DEFAULT_TIME
 ): (() => void) => {
@@ -19,5 +19,3 @@ const useThrottle = <T extends Function>(
 		callback();
 	};
 };
-
-export default useThrottle;
