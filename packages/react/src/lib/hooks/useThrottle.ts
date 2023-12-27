1886ed87bcb33626fd0d05c6368c326d653bdf39
EunBae Gong <geb9211@gmail.com>
2023-12-27 20:22:16 +0900

    feat: 함수에서 params를 받도록 변경

--- packages/react/src/lib/hooks/useThrottle.ts
+++ packages/react/src/lib/hooks/useThrottle.ts
@@ -4,18 +4,18 @@ import { useRef } from "react";
 
 const THROTTLE_DEFAULT_TIME = 1 * 1000;
 
-export const useThrottle = <T extends Function>(
-	callback: T,
+export const useThrottle = <T extends unknown[]>(
+	callback: (...params: T) => void,
 	throttleTime: number | undefined = THROTTLE_DEFAULT_TIME
 ): (() => void) => {
 	const timer = useRef<ReturnType<Date["valueOf"]>>(0);
 
-	return () => {
+	return (...params: T) => {
 		const callbackExecutionTime = new Date().valueOf();
 
 		if (callbackExecutionTime - timer.current < throttleTime) return;
 
 		timer.current = callbackExecutionTime;
-		callback();
+		callback(...params);
 	};
 };
