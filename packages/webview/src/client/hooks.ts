f1f7d0e9d097192eef472b18f5558ecaba3a9bac
EunBae Gong <geb9211@gmail.com>
2024-01-24 23:39:33 +0900

    feat: native client용 함수 및 훅 작성

---
+++ packages/webview/src/client/hooks.ts
@@ -0,0 +1,28 @@
+import WebView from "react-native-webview";
+import { useEffect, useRef } from "react";
+import { BackHandler, Platform } from "react-native";
+
+const useWebview = () => {
+	const webViewRef = useRef<WebView>();
+	const onAndroidBackPress = () => {
+		if (webViewRef.current) {
+			webViewRef.current.goBack();
+			return true; // prevent default behavior (exit app)
+		}
+		return false;
+	};
+
+	useEffect(() => {
+		if (Platform.OS === "android") {
+			BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+			return () => {
+				BackHandler.removeEventListener(
+					"hardwareBackPress",
+					onAndroidBackPress
+				);
+			};
+		}
+	}, []);
+};
+
+export default useWebview;
---
+++ packages/webview/src/client/index.ts
@@ -0,0 +1,28 @@
+import WebView, { WebViewMessageEvent } from "react-native-webview";
+import { Protocol } from "../protocols";
+import { ProtocolPayloadType } from "../protocols/types";
+
+type OnMessageFromWebViewProps = WebViewMessageEvent &
+	ProtocolPayloadType & {
+		webviewRef: React.MutableRefObject<WebView<object> | null>;
+	};
+
+export const onMessageFromWebView = ({
+	nativeEvent,
+	webviewRef,
+	userPayload,
+	accessTokenPayload,
+}: OnMessageFromWebViewProps) => {
+	const protocol = new Protocol(JSON.parse(nativeEvent.data));
+	switch (protocol.name) {
+		case "user.accessToken":
+			webviewRef.current?.postMessage(
+				JSON.stringify({ ...protocol, payload: { ...accessTokenPayload } })
+			);
+			return;
+		case "user.userInfo":
+			webviewRef.current?.postMessage(
+				JSON.stringify({ ...protocol, payload: { ...userPayload } })
+			);
+	}
+};
