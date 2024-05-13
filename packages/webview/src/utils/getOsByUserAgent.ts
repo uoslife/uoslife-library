78225f1c2c37ea3825bca6deec6dd75f8b7f4006
EunBae Gong <geb9211@gmail.com>
2024-05-13 20:53:10 +0900

    feat: getOSByUserAgent 함수 추가

---
+++ packages/webview/src/utils/getOsByUserAgent.ts
@@ -0,0 +1,19 @@
+import { isAndroid } from "./isAndroid";
+import { isIOS } from "./isIOS";
+import { isServer } from "./isServer";
+
+export function getOSByUserAgent() {
+	if (isServer()) {
+		return false;
+	}
+
+	if (isIOS()) {
+		return "ios";
+	}
+
+	if (isAndroid()) {
+		return "android";
+	}
+
+	return "web";
+}
